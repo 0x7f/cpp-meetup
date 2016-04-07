2f38e2e057d7b55ecb5d832091531466806af1c5
Maximilian Haupt <mail@maximilianhaupt.com>
2016-04-07 13:25:13 +0200

    Add schema-validation to rapidjson test case.

--- tools/rapidjson/main.cpp
+++ tools/rapidjson/main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <chrono>
+#include <fstream>
 #include <thread>
 
 #include <boost/log/core.hpp>
@@ -13,19 +14,48 @@
 #include <commonpp/thread/ThreadPool.hpp>
 
 #include <rapidjson/document.h>
+#include <rapidjson/filereadstream.h>
+#include <rapidjson/reader.h>
+#include <rapidjson/schema.h>
+#include <rapidjson/stream.h>
 
-void connection_handler(HTTPP::HTTP::Connection* connection) {
-    read_whole_request(connection, [](std::unique_ptr<HTTPP::HTTP::helper::ReadWholeRequest> handle,
+using namespace ::rapidjson;
+
+static std::string getFileContent(const std::string& path) {
+  std::ifstream file(path);
+  return std::string{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+}
+
+void connection_handler(SchemaValidator* sv, HTTPP::HTTP::Connection* connection) {
+    read_whole_request(connection, [sv](std::unique_ptr<HTTPP::HTTP::helper::ReadWholeRequest> handle,
                                       const boost::system::error_code& ec) {
         if (ec) {
             throw HTTPP::UTILS::convert_boost_ec_to_std_ec(ec);
         }
 
-        const auto& body = handle->body;
+        auto& body = handle->body;
+        body.push_back('\0');
         auto& connection = handle->connection;
         auto& response = connection->response();
 
-        rapidjson::Document d;
+        Reader reader;
+        StringStream ss(body.data());
+        sv->Reset();
+        if (!reader.Parse(ss, *sv) && reader.GetParseErrorCode() != kParseErrorTermination) {
+            response.setCode(HTTPP::HTTP::HttpCode::BadRequest);
+            response.connectionShouldBeClosed(true);
+            connection->sendResponse(); // connection pointer may become invalid
+            return;
+        }
+
+        if (!sv->IsValid()) {
+            response.setCode(HTTPP::HTTP::HttpCode::BadRequest);
+            response.connectionShouldBeClosed(true);
+            connection->sendResponse(); // connection pointer may become invalid
+            return;
+        }
+
+        Document d;
         try {
             d.Parse(body.data(), body.size());
             if (d.HasMember("id")) {
@@ -67,9 +97,21 @@ int main() {
 
     commonpp::thread::ThreadPool threadPool{static_cast<size_t>(numThreads)};
 
+    const auto path = "share/openrtb-schema_bid-request_v2-3.json";
+    const auto content = getFileContent(path);
+    Document d;
+    d.Parse(content.c_str(), content.size());
+    if (d.HasParseError()) {
+        fprintf(stderr, "Schema file '%s' is not a valid JSON\n", path);
+        return 1;
+    }
+
+    SchemaDocument sd(d);
+    SchemaValidator sv(sd);
+
     HTTPP::HttpServer server{threadPool};
     server.start();
-    server.setSink(&connection_handler);
+    server.setSink(std::bind(&connection_handler, &sv, std::placeholders::_1));
     server.bind(host, port);
 
     printf("READY\n");
