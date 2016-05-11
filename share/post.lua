a0731b6ee1587eded130010917cb5fbd9d0c3f3d
Maximilian Haupt <mail@maximilianhaupt.com>
2016-05-11 10:59:15 +0200

    Add two example requests.
    Copied from rtbkit test suite and stripped down to get a small version.

--- share/post.lua
+++
@@ -1,3 +0,0 @@
-wrk.method = "POST"
-wrk.body   = "{}"
-wrk.headers["Content-Type"] = "application/json"
---
+++ share/post_128.lua
@@ -0,0 +1,3 @@
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/json"
+wrk.body = '{"id":"1234534","imp":[{"id":"1"}],"site":{"id":"234563","domain":"siteabcd.com"},"device":{"ip":"64.124.253.1"},"cur":["USD"]}'
