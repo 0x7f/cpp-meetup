7cf720421a0cb0feaa7d751914961161231d0b2a
Maximilian Haupt <mail@maximilianhaupt.com>
2016-05-17 10:06:25 +0200

    Reorder thirdparty libraries to prevent linker errors.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,11 +2,25 @@ set(THIRDPARTY_LIBRARIES)
 
 include(${CMAKE_CURRENT_LIST_DIR}/LibFindMacros.cmake)
 
+# daedric's httpp
+include_directories(${CMAKE_SOURCE_DIR}/thirdparty/httpp/include/)
+include_directories(${CMAKE_SOURCE_DIR}/thirdparty/httpp/build/include/)
+include_directories(${CMAKE_SOURCE_DIR}/thirdparty/httpp/third_party/commonpp/include/)
+include_directories(${CMAKE_SOURCE_DIR}/thirdparty/httpp/third_party/commonpp/build/include/)
+
+list(APPEND THIRDPARTY_LIBRARIES ${CMAKE_SOURCE_DIR}/thirdparty/httpp/build/src/httpp/libhttpp.a)
+list(APPEND THIRDPARTY_LIBRARIES ${CMAKE_SOURCE_DIR}/thirdparty/httpp/third_party/commonpp/build/src/commonpp/libcommonpp.a)
+
+# OpenSSL
+find_package(OpenSSL REQUIRED)
+include_directories(${OPENSSL_INCLUDE_DIR})
+list(APPEND THIRDPARTY_LIBRARIES ${OPENSSL_LIBRARIES})
+
 # boost
 set(Boost_USE_STATIC_LIBS        ON)
 set(Boost_USE_MULTITHREADED      ON)
 set(Boost_USE_STATIC_RUNTIME    OFF)
-find_package(Boost 1.48.0 REQUIRED COMPONENTS date_time filesystem system log thread)
+find_package(Boost 1.48.0 REQUIRED COMPONENTS date_time log filesystem system thread)
 include_directories(${Boost_INCLUDE_DIRS})
 list(APPEND THIRDPARTY_LIBRARIES ${Boost_LIBRARIES})
 
@@ -15,18 +29,10 @@ find_package(Protobuf REQUIRED)
 include_directories(${PROTOBUF_INCLUDE_DIRS})
 list(APPEND THIRDPARTY_LIBRARIES ${PROTOBUF_LIBRARIES})
 
-# Google's Flatbuffersj
-list(APPEND THIRDPARTY_LIBRARIES /usr/local/lib/libflatbuffers.a)
-
 # curl
 find_package(CURL REQUIRED)
 include_directories(${CURL_INCLUDE_DIRS})
 list(APPEND THIRDPARTY_LIBRARIES ${CURL_LIBRARIES})
-
-# OpenSSL
-find_package(OPENSSL REQUIRED)
-include_directories(${OPENSSL_INCLUDE_DIR})
-list(APPEND THIRDPARTY_LIBRARIES ${OPENSSL_LIBRARIES})
 
 # Open MPI's Portable Hardware Locality (required by httpp)
 include(${CMAKE_CURRENT_LIST_DIR}/FindHWLOC.cmake)
@@ -37,12 +43,3 @@ list(APPEND THIRDPARTY_LIBRARIES ${HWLOC_LIBRARIES})
 include(${CMAKE_CURRENT_LIST_DIR}/FindTBB.cmake)
 include_directories(${TBB_INCLUDE_DIRS})
 list(APPEND THIRDPARTY_LIBRARIES ${TBB_LIBRARIES})
-
-# daedric's httpp
-include_directories(${CMAKE_SOURCE_DIR}/thirdparty/httpp/include/)
-include_directories(${CMAKE_SOURCE_DIR}/thirdparty/httpp/build/include/)
-include_directories(${CMAKE_SOURCE_DIR}/thirdparty/httpp/third_party/commonpp/include/)
-include_directories(${CMAKE_SOURCE_DIR}/thirdparty/httpp/third_party/commonpp/build/include/)
-
-list(APPEND THIRDPARTY_LIBRARIES ${CMAKE_SOURCE_DIR}/thirdparty/httpp/build/src/httpp/libhttpp.a)
-list(APPEND THIRDPARTY_LIBRARIES ${CMAKE_SOURCE_DIR}/thirdparty/httpp/third_party/commonpp/build/src/commonpp/libcommonpp.a)
