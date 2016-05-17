a251032e2cb82ee017c4a4b716b6583dd5d649a1
Maximilian Haupt <mail@maximilianhaupt.com>
2016-05-17 17:25:03 +0200

    Improve demo2 and demo3.

--- demo/demo2.cpp
+++ demo/demo2.cpp
@@ -7,32 +7,33 @@
 using namespace ::com::google::openrtb;
 
 int main() {
-    BidRequest bidRequest;
-    bidrequest_parser_state_t state = bidrequest_parser_init(bidRequest);
+  BidRequest bidRequest;
+  bidrequest_parser_state_t state = bidrequest_parser_init(bidRequest);
 
-    char buf[1024];
-    int rc, bytesRead;
-    while((bytesRead = read(STDIN_FILENO, buf, sizeof(buf) / sizeof(buf[0]))) > 0) {
-        rc = bidrequest_parser_on_chunk(state, buf, bytesRead);
-        if (rc != 0) {
-            char* err = bidrequest_parser_get_error(state);
-            fprintf(stderr, "Error: %s\n", err);
-            bidrequest_parser_free_error(state, err);
-            return EXIT_FAILURE;
-        }
-    }
-
-    rc = bidrequest_parser_complete(state);
+  char buf[1024];
+  int rc, bytesRead;
+  while ((bytesRead = read(STDIN_FILENO, buf, sizeof(buf) / sizeof(buf[0]))) >
+         0) {
+    rc = bidrequest_parser_on_chunk(state, buf, bytesRead);
     if (rc != 0) {
-        char* err = bidrequest_parser_get_error(state);
-        fprintf(stderr, "Error: %s\n", err);
-        bidrequest_parser_free_error(state, err);
-        return EXIT_FAILURE;
+      char *err = bidrequest_parser_get_error(state);
+      fprintf(stderr, "Error: %s\n", err);
+      bidrequest_parser_free_error(state, err);
+      return EXIT_FAILURE;
     }
+  }
 
-    bidrequest_parser_free(state);
+  rc = bidrequest_parser_complete(state);
+  if (rc != 0) {
+    char *err = bidrequest_parser_get_error(state);
+    fprintf(stderr, "Error: %s\n", err);
+    bidrequest_parser_free_error(state, err);
+    return EXIT_FAILURE;
+  }
 
-    printf("%s\n", bidRequest.DebugString().c_str());
+  bidrequest_parser_free(state);
 
-    return EXIT_SUCCESS;
+  printf("%s\n", bidRequest.DebugString().c_str());
+
+  return EXIT_SUCCESS;
 }
