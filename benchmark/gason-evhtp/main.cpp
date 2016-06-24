5c228037b3ea1858cec3d4b3f4192cdc02295cc9
Maximilian Haupt <mail@maximilianhaupt.com>
2016-06-24 11:10:49 +0200

    Port gason-httpp to evhtp.

---
+++ benchmark/gason-evhtp/main.cpp
@@ -0,0 +1,49 @@
+#include <gason.h>
+
+#include "evhtp_util.h"
+#include "parse_env.h"
+
+static void add_cb(evhtp_request_t* req, void* a) {
+    if (req->method != htp_method_POST) {
+        evhtp_send_reply(req, EVHTP_RES_METHNALLOWED);
+        return;
+    }
+
+    size_t len;
+    auto buf = extract_body(req, &len);
+    if (!buf) {
+        evhtp_send_reply(req, EVHTP_RES_500);
+        return;
+    }
+
+    char* endptr;
+    JsonValue value;
+    JsonAllocator allocator;
+    int status = jsonParse(buf, &endptr, &value, allocator);
+    if (status != JSON_OK) {
+        evhtp_send_reply(req, EVHTP_RES_500);
+        return;
+    }
+
+    evbuffer_add(req->buffer_out, "OK:", 3);
+    // TODO: how to retrieve id?
+    // evbuffer_add(req->buffer_out, id.c_str(), id.size());
+    evhtp_send_reply(req, EVHTP_RES_OK);
+}
+
+int main() {
+    const char* host;
+    uint16_t port, num_threads;
+    bool skip_validation;
+    parse_env(&host, &port, &num_threads, &skip_validation);
+
+    evbase_t* evbase = event_base_new();
+    evhtp_t* htp = evhtp_new(evbase, NULL);
+    evhtp_set_cb(htp, "/add", add_cb, NULL);
+    evhtp_use_threads_wexit(htp, init_thread, exit_thread, num_threads, NULL);
+    evhtp_bind_socket(htp, host, port, 1024);
+
+    printf("READY\n");
+
+    event_base_loop(evbase, 0);
+}
